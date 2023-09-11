73b5a5ff878129c7291826335bdc1a0e7ed2e6b1
Jayavenkate <jayajaikithuja@gmail.com>
2023-09-11 16:30:16 +0300

    cors

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import cors from "cors";
 import usersRouter from "./router/users.router.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import nodemailer from "nodemailer";
+
 
 const app = express();
 //http://localhost:8000
@@ -67,69 +65,5 @@ app.post("/createscreen", async function (request, response) {
 
   response.send(result);
 });
-// async function generateHashedPassword(password) {
-//   const NO_OF_ROUNDS = 10;
-//   const salt = await bcrypt.genSalt(NO_OF_ROUNDS);
-//   const hashedPassword = await bcrypt.hash(password, salt);
-//   console.log(salt);
-//   console.log(hashedPassword);
-//   return hashedPassword;
-// }
-// app.post("/signup", async function (request, response) {
-//   try {
-//     const { name, email, password } = request.body;
-//     const userFromDb = await client
-//       .db("b42wd2")
-//       .collection("users")
-//       .findOne({ email: email });
-//     console.log(userFromDb);
-//     if (userFromDb) {
-//       response.status(401).send({ message: "email already exists" });
-//     } else if (password.length < 8) {
-//       response
-//         .status(400)
-//         .send({ message: "Password must be at least 8 characters" });
-//     } else {
-//       const hashedPassword = await generateHashedPassword(password);
-//       const result = await client.db("b42wd2").collection("users").insertOne({
-//         name: name,
-//         email: email,
-//         password: hashedPassword,
-//       });
-//       response.send(result);
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
-
-// app.post("/login", async function (request, response) {
-//   try {
-//     const { email, password } = request.body;
-//     const userFromDb = await client
-//       .db("b42wd2")
-//       .collection("users")
-//       .findOne({ email: email });
-//     console.log(userFromDb);
-//     if (!userFromDb) {
-//       response.status(401).send({ message: "Invalid credentials" });
-//     } else {
-//       const storedDBPassword = userFromDb.password;
-//       const isPasswordCheck = await bcrypt.compare(password, storedDBPassword);
-//       console.log(isPasswordCheck);
-//       if (isPasswordCheck) {
-//         const token = jwt.sign({ id: userFromDb._id }, process.env.SECRET_KEY);
-//         response
-//           .status(200)
-//           .send({ message: "successful login", token: token });
-//       } else {
-//         response.status(401).send({ message: "Invalid credentials" });
-//       }
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
-
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
